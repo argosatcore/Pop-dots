c6518208308dd36f9805d9c56f1ebb5f75891300
Carlos Aguilar <argos@pop-os.localdomain>
2021-04-14 14:08:05 -0600

    Added boostrapping script.

---
+++ Popstrap/popstrap.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#Bootstrapping script for Debian-based systems:
+echo 'Initializing bootstrap'
+echo ' '
+echo 'Updating system...'
+echo ' '
+sudo apt update && \
+sudo apt upgrade -y
+echo ' '
+echo 'Installing commonly used packages...'
+sudo apt install -y \
+	blender \
+	default-jdk \
+	deja-dup \
+	ffmpeg \
+	fzf \
+	gimp \
+	git \
+	gpick \
+	gucharmap \
+	htop \
+	inkscape \
+	librecad \
+	libreoffice-common \
+	lm-sensors \
+	lollypop \
+	neofetch \
+	neovim \
+	nnn \
+	pavucontrol \
+	scribus \
+	tmux \
+	texlive-full \
+	vim-gtk \
+	vlc \
+	youtube-dl
+echo 'Installing Zotero...'
+echo ' ' 
+flatpak install flathub org.zotero.Zotero
+echo ' '
+echo 'Capturing dotfiles...'
+echo ' '
+git clone 
+echo ' '
+echo 'Bootstrapping complete. Welcome back, Argos.'
